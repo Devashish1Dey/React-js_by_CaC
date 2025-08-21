3f4aa27e56f6b03fd3df80ca189709f18decd4c6
Devashish1Dey <devashishdey01@gmail.com>
2025-08-21 18:39:20 +0530

    Understanding useState with small counter problems

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+let [counter, setCounter] = useState(5)
+
+// let counter = 5
+
+const addValue = ()=>{
+  // console.log("Clicked", Math.random())
+  counter = counter + 1
+  setCounter(counter)
+  // if(counter >= 20){
+      // console.log("Clicked", 20)
+  // }else{
+    console.log("Clicked", counter)
+  // }
+}
+
+const removeValue = ()=>{
+  counter = counter - 1
+  setCounter(counter)
+  // if(counter <=0){
+    
+  // }
+  // console.log("clicked", counter)
+}
+
+  return (
+    <>
+      <h1>Chai or react</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
